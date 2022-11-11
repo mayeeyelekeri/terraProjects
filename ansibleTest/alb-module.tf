12aa84ccf0126f152bf1bfe97556f031f3896248
Yelekeri <yelekeri@gmail.com>
2022-11-11 18:53:36 -0500

    ansible

--- ansibleTest/alb-module.tf
+++ ansibleTest/alb-module.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "jenkins-server" {
   subnet_id                   = values(aws_subnet.public-subnets)[0].id  # get the first subnet id 
   
   provisioner "remote-exec" {
-    inline = ["echo Done!"]
+    inline = ["sudo yum update -y", "sudo yum install python3 -y", "echo Done!"]
 
     connection {
       host        = self.public_ip
@@ -41,41 +41,6 @@ EOF
     Environment = "${terraform.workspace}"
   }
 }
-
-# Create EC2 web servers in different subnets 
-resource "aws_instance" "webservers" {
-  for_each                    = aws_subnet.public-subnets
-  ami                         = var.ami-id
-  instance_type               = var.instance-type
-  associate_public_ip_address = true
-  key_name                    = var.key-name
-  vpc_security_group_ids      = [aws_security_group.public-sg.id] 
-  subnet_id                   = each.value.id
-  provisioner "remote-exec" {
-    #inline = ["sudo yum update -y", "sudo yum install httpd -y", "sudo systemctl start httpd", "echo Done!"]
-    inline = [
-      "sudo yum -y install httpd && sudo systemctl start httpd",
-      "echo '<html><body><h1><center> My public IP address is : </center></h1>' > index.html", 
-      "echo '<h2  style=\"background-color:' >> index.html",
-      "echo ${var.ec2-data[each.key].color} >> index.html", 
-      "echo ';\"><center> ' >> index.html",
-      "curl http://169.254.169.254/latest/meta-data/public-ipv4 >> index.html",
-      "echo '</h2></center> </body> </html>' >> index.html",
-      "sudo mv index.html /var/www/html/"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
-  } 
-
-  tags = {
-    Name = join("-", ["${terraform.workspace}", "webserver" ])
-    Environment = "${terraform.workspace}"
-  }
-}   
 
 # Create EC2 web servers in different subnets 
 resource "aws_instance" "http-server" {
