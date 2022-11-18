efabde0739e593ec70d74665e28d43848a19aae1
Yelekeri <yelekeri@gmail.com>
2022-11-18 11:28:20 -0500

    codedeploy

--- codeDeploy/code-deploy-module.tf
+++ codeDeploy/code-deploy-module.tf
@@ -58,7 +58,7 @@ resource "aws_iam_instance_profile" "myinstanceprofile" {
 
 # Attach policy to role 
 resource "aws_iam_role_policy_attachment" "roll_attach_to_policy" {
-  role       = aws_iam_instance_profile.myinstanceprofile.name
+  role       = aws_iam_role.myec2role.name
   policy_arn = aws_iam_policy.mys3policy.arn
 }
 
