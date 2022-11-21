b51e7a39afc036a63c65c76abde7ad64b10845fd
Yelekeri <yelekeri@gmail.com>
2022-11-21 17:55:36 -0500

    codepipeline

--- elasticBeanStack/ebs-module.tf
+++ elasticBeanStack/ebs-module.tf
@@ -36,7 +36,7 @@ resource "aws_elastic_beanstalk_environment" "myapp-env" {
    // value = "aws-elasticbeanstalk-ec2-role"  **** this gets created automatically from aws console when an app is created 
   }
 
-  depends_on = [aws_iam_role.beanstackrole]
+  depends_on = [aws_iam_role.beanstackrole, aws_iam_instance_profile.myinstanceprofile]
 }
 
 # Create s3 bucket 
