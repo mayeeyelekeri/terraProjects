3d875104d8057778deb22d64fd83153865289ec5
Yelekeri <yelekeri@gmail.com>
2022-11-22 10:39:58 -0500

    codepipeline

--- springBoot/ec2-module.tf
+++ springBoot/ec2-module.tf
@@ -68,7 +68,7 @@ resource "aws_iam_role_policy_attachment" "roll_attach_to_policy" {
 
 # Install Httpd and start at port 80 
 # Create EC2 web servers in different subnets 
-resource "aws_instance" "http-server" {
+resource "aws_instance" "web-server" {
   for_each                    = aws_subnet.public-subnets
   ami                         = var.ami-id
   instance_type               = var.instance-type
@@ -77,13 +77,6 @@ resource "aws_instance" "http-server" {
   key_name                    = var.key-name
   vpc_security_group_ids      = [aws_security_group.public-sg.id] 
   subnet_id                   = each.value.id
-
-  # Install httpd 
-  provisioner "local-exec" {
-    command = <<EOF
-aws ec2 wait instance-status-ok --instance-ids ${self.id} && ansible-playbook --extra-vars "passed_in_hosts=${self.public_ip}" ansible_templates/install_codedeploy_agent.yaml
-EOF
-  } # End of provisioner
 
   # Install java and copy springboot war file 
   provisioner "local-exec" {
