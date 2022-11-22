72ed019191a6d024a14557051432af619d7bb0f7
Yelekeri <yelekeri@gmail.com>
2022-11-21 19:36:23 -0500

    codepipeline

--- elasticBeanStack/init.sh
+++ elasticBeanStack/init.sh
@@ -2,14 +2,10 @@
 --- Create an S3 bucket for strong terraform state 
 aws s3 mb s3://terraprojects --region us-east-1
 
---- Make sure the web application source directory exists at this location where the terraform is running from 
-/home/vagrant/webapp 
+--- Make sure the spring boot web application source directory exists at this location where the terraform is running from 
+/home/vagrant/springdemo/target 
 
 --- Make sure "ansible" is installed 
-
---- Key-pair is from the local machine
-
---- Create "codepipeline" project by using existing service role with "s3" as source and "codedeploy" as target 
 
 --- To execute 
 terraform init -reconfigure 
