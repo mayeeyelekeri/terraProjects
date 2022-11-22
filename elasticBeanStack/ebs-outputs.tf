860bce7aca81261bf013325145b5ba2732af3020
Yelekeri <yelekeri@gmail.com>
2022-11-21 22:29:33 -0500

    codepipeline

--- elasticBeanStack/ebs-outputs.tf
+++ elasticBeanStack/ebs-outputs.tf
@@ -2,6 +2,10 @@ output "elastic_Bean_Stack_application" {
 	value	  = aws_elastic_beanstalk_application.mywebapp
 } 
 
+output "elastic_Bean_Stack_docker_application" {
+	value	  = aws_elastic_beanstalk_application.dockerapp
+} 
+
 output "bucket" {
 	value = aws_elastic_beanstalk_application_version.beanstalk_myapp_version.name
 }
