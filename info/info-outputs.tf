b22dd4f521d720a5b51881098b07930444c11083
Yelekeri <yelekeri@gmail.com>
2022-11-23 15:24:42 -0500

    info application, all taken care

--- info/info-outputs.tf
+++ info/info-outputs.tf
@@ -6,6 +6,11 @@ output "info-server-ipaddress" {
 	value = aws_instance.info_server.public_ip
 }
 
+output "info-server-dns" {
+	value = aws_instance.info_server.dns
+}
+
 output "info-client-ipaddress" {
 	value = aws_instance.info_client.public_ip
 }
+
