158da6aa4f25ff5efc912b5f774669723a7e4af1
Yelekeri <yelekeri@gmail.com>
2022-11-25 17:57:37 -0500

    infoWeb

--- infoWeb/info-server-module.tf
+++ infoWeb/info-server-module.tf
@@ -5,7 +5,7 @@ data "aws_secretsmanager_secret_version" "creds" {
 }
 
 locals {
-  mysql_creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
+  mysql_creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_id)
 }
 
 # Get database endpoint and update infoserver application-aws.properties 
