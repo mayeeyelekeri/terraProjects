a5df82f972f190e7b9ab3a58e24e62d14275a483
Yelekeri <yelekeri@gmail.com>
2022-11-25 23:38:15 -0500

    infoWeb

--- infoWeb/init.sh
+++ infoWeb/init.sh
@@ -2,6 +2,9 @@
 --- Create an S3 bucket for strong terraform state 
 aws s3 mb s3://terraprojects --region us-east-1
 
+--- In database directory, execute script to create creds in secrets manager 
+. ~/INFO/secrets/aws_secrets.sh 
+
 --- Make sure the web application source directory exists at this location where the terraform is running from 
 /home/vagrant/SpringDataTest and /home/vagrant/Client exists 
 
@@ -12,8 +15,6 @@ aws s3 mb s3://terraprojects --region us-east-1
 --- This project is going to create 2 ec2s - one for server and other for client. 
 
 --- This project depends on database is already created with data  
-
---- In Server module, make sure "TF_VAR_infodb_endpoint" variable is set with actual db endpoint
 
 --- In Client module, update Servers IP address at application.properties 
 
