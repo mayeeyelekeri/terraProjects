3d0abc8d0d1c08ace7b6438c303bff4bc8234aac
Yelekeri <yelekeri@gmail.com>
2022-11-29 23:08:53 -0500

    infoweb

--- infoWeb/info-server-module.tf
+++ infoWeb/info-server-module.tf
@@ -64,8 +64,8 @@ resource "aws_launch_configuration" "al_conf" {
 resource "aws_autoscaling_group" "sc_group" {
   name                 = "my_asg"
   launch_configuration = aws_launch_configuration.al_conf.name
-  load_balancers       = [aws_lb.alb.id]
-  #target_group_arns   = aws_lb_target_group.tg
+  #load_balancers      = [aws_lb.alb.id]
+  target_group_arns    = [aws_lb.alb.target_group_arns]
   # availability_zones = ["us-east-1a" , "us-east-1b"]
   vpc_zone_identifier  = [values(aws_subnet.public_subnets)[0].id, values(aws_subnet.public_subnets)[1].id]
   min_size             = 2
