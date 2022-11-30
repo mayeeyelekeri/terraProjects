a48581a8314dea0813ac890b06fdeaaf38a83a42
Yelekeri <yelekeri@gmail.com>
2022-11-30 16:07:39 -0500

    infoweb

--- infoWeb/code-deploy-module-client.tf
+++ infoWeb/code-deploy-module-client.tf
@@ -1,7 +1,7 @@
 #.................................................
 # Create Code Deploy application 
 resource "aws_codedeploy_app" "myapp_client" {
-  name = var.my_app_client
+  name = var.app_name_client
 
   depends_on = [aws_iam_role_policy_attachment.codedeploy_service]
 }
