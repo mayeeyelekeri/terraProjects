6bcf3b0bc6c5564cfa07865b8782925f7e185907
Yelekeri <yelekeri@gmail.com>
2022-11-30 23:08:07 -0500

    infoweb

--- infoWeb/awsStorage.tf
+++ infoWeb/awsStorage.tf
@@ -10,13 +10,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-module "vpc" {
-    source = "./vpc"
-
-    # Pass all the variable values to the vpc module 
-    aws_region        = var.aws_region
-    open_cidr         = var.open_cidr 
-    vpc_cidr          = var.vpc_cidr 
-    public_subnet_map = var.public_subnet_map
-}
