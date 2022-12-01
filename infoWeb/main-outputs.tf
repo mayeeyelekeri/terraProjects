1cfea48605efc4d9a57c3d990935ab342e100ced
Yelekeri <yelekeri@gmail.com>
2022-12-01 10:16:44 -0500

    module creation

---
+++ infoWeb/main-outputs.tf
@@ -0,0 +1,12 @@
+output "vpc_id" {
+  description = "VPC ID created"
+  value       = module.vpc.vpc_id
+}
+
+output "public_sg" {
+	value = "module.vpc.public_sg"
+}
+
+output "public_sg_id" {
+	value = "module.vpc.public_sg_id"
+}
