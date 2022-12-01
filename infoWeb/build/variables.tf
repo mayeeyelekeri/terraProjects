d34c17d5366da7c1b6d2c74ee9ca0d428ed48cbf
Yelekeri <yelekeri@gmail.com>
2022-12-01 12:21:08 -0500

    module creation

--- infoWeb/build/variables.tf
+++ infoWeb/build/variables.tf
@@ -1,6 +1,14 @@
+
+variable "mysql_creds" {}
+
 variable "webapp_src_location_server" {} 
 variable "dest_properties_file_server" {} 
 variable "src_properties_file_server" {}
-variable "mysql_creds" {}
 variable "info_server_workspace" {}
 variable "jar_file_server" {} 
+
+variable "webapp_src_location_client" {} 
+variable "dest_properties_file_client" {} 
+variable "src_properties_file_client" {}
+variable "info_server_client" {}
+variable "jar_file_client" {} 
