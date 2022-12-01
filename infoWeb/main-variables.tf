5a64f7fd41d4d124081ce462d22bc5359fa962a1
Yelekeri <yelekeri@gmail.com>
2022-12-01 10:55:29 -0500

    module creation

--- infoWeb/main-variables.tf
+++ infoWeb/main-variables.tf
@@ -23,6 +23,6 @@ variable "zip_file_server" {}
 variable "zip_file_client" {}
 variable "webapp_src_location_server" {}
 variable "webapp_src_location_client" {}
-variable "auto_scale_group_name_server" {}
-variable "auto_scale_group_name_client" {}
+#variable "auto_scale_group_name_server" {}
+#variable "auto_scale_group_name_client" {}
 
