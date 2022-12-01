332a6863ea0075a51b547ef53511cfedecc159b9
Yelekeri <yelekeri@gmail.com>
2022-12-01 10:24:03 -0500

    module creation

--- infoWeb/main-outputs.tf
+++ infoWeb/main-outputs.tf
@@ -1,3 +1,4 @@
+# -------------- VPC outputs -----------------
 output "vpc_id" {
   description = "VPC ID created"
   value       = module.vpc.vpc_id
@@ -5,4 +6,22 @@ output "vpc_id" {
 
 output "public_sg_id" {
 	value = module.vpc.public_sg_id
+}
+
+# ------------ ALB outputs -------------------
+
+output "alb_tg_server_arn" { 
+	value = module.alb.alb_tg_server_arn
+}
+
+output "alb_tg_client_arn" { 
+	value = module.alb.alb_tg_client_arn
+}
+
+output "alb_server_dns" { 
+	value = module.alb.alb_server_dns
+}
+
+output "alb_client_dns" { 
+	value = module.alb.alb_client_dns
 }
