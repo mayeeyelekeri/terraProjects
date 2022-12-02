a19bb0bdc3e648bd6a67f9809735075522b64552
Yelekeri <yelekeri@gmail.com>
2022-12-02 15:00:10 -0500

    private

---
+++ modules/vpc/vpc-groups.tf
@@ -0,0 +1,93 @@
+# -------------------- Public Security Groups --------------
+# Create SG for allowing TCP/80 & TCP/22
+resource "aws_security_group" "public_sg" {
+  name        = "${terraform.workspace}-public-sg"
+  description = "Allow TCP/80 & TCP/22"
+  vpc_id      = aws_vpc.myvpc.id
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.open_cidr]
+  }
+  ingress {
+    description = "allow traffic from TCP/80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.open_cidr]
+  }
+  ingress {
+    description = "allow traffic from TCP/8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [var.open_cidr]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "${terraform.workspace}-Public-Sec-Group"
+    Environment = "${terraform.workspace}"
+  }
+
+  depends_on = [aws_vpc.myvpc , aws_route_table.internet_route, aws_subnet.public_subnets ]
+}
+
+/* 
+# Private Security Group
+resource "aws_security_group" "private-sg" {
+  name        = "${terraform.workspace}-private-sg"
+  description = "Allow all access from public subnet"
+  vpc_id      = aws_vpc.myvpc.id
+  ingress {
+    description = "Allow http from public subnet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.mysubnet["public"].cidr_block]
+  } 
+  ingress {
+    description = "Allow http from public subnet"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.mysubnet["public"].cidr_block]
+  } 
+  ingress {
+    description = "Allow ssh from  public subnet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.mysubnet["public"].cidr_block]
+  } 
+  ingress {
+    description = "Allow ping  from public subnet"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_subnet.mysubnet["public"].cidr_block]
+  } 
+  # ------- By default terraform doesn't create this block ----- 
+  # ------- I spent lot of time to investigate this ------------
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${terraform.workspace}-Private-Security-Group"
+    Environment = "${terraform.workspace}"
+  }
+
+  depends_on = [aws_vpc.myvpc , aws_route_table.internet_route, aws_subnet.private_subnets ]
+} */ 
