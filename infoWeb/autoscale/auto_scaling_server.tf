05da5afd2f98ca2015da3f27f3ce66b6ca91591f
Yelekeri <yelekeri@gmail.com>
2022-12-02 09:43:50 -0500

    module creation

--- infoWeb/autoscale/auto_scaling_server.tf
+++ infoWeb/autoscale/auto_scaling_server.tf
@@ -16,8 +16,23 @@ data "aws_ssm_parameter" "linux-ami" {
     name        = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
+resource "aws_launch_template" "docker_template" {
+  name                 = "docker_and_codedeploy_agent"
+  name_prefix          = "${terraform.workspace}_"
+  image_id             = var.ami_id
+  instance_type        = var.instance_type
+  key_name             = var.key_name
+  security_groups      = [var.public_sg_id] 
+  user_data            = "${data.template_file.user_data.rendered}"
+  iam_instance_profile = var.instance_profile_name 
+
+  lifecycle {
+    create_before_destroy = true
+  } 
+}
+
 # Create Launch configuration (This one is common for both client and server)
-resource "aws_launch_configuration" "al_conf" {
+/*resource "aws_launch_configuration" "al_conf" {
   name_prefix          = "${terraform.workspace}_"
   image_id             = var.ami_id
   instance_type        = var.instance_type
@@ -32,11 +47,16 @@ resource "aws_launch_configuration" "al_conf" {
 
   #depends_on = [aws_iam_instance_profile.myinstanceprofile , aws_lb_listener.listener]
 }
+*/ 
 
 # Create Auto Scaling group 
 resource "aws_autoscaling_group" "auto_scale_group" {
   name                 = var.app_name_server
-  launch_configuration = aws_launch_configuration.al_conf.name
+  #launch_configuration = aws_launch_configuration.al_conf.name
+  launch_template      = {
+        id      = aws_launch_template.docker_template.id 
+        version = "$$Latest" 
+  } 
   target_group_arns    = [var.alb_tg_server_arn]
   vpc_zone_identifier  = [var.public_subnets[0].id, var.public_subnets[1].id]
   health_check_type    = "EC2" 
--- infoWeb/main.tf
+++ infoWeb/main.tf
@@ -61,8 +61,8 @@ module "autoscale" {
     ami_id                = var.ami_id 
     instance_type         = var.instance_type 
     instance_profile_name = var.instance_profile_name
-    autoscale_min        = var.autoscale_min 
-    autoscale_max        = var.autoscale_max 
+    autoscale_min         = var.autoscale_min 
+    autoscale_max         = var.autoscale_max 
 
     # from VPC module 
     public_sg_id          = module.vpc.public_sg_id
