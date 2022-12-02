347a71b60c0a574d6fda7a317764dc4166569e3b
Yelekeri <yelekeri@gmail.com>
2022-12-02 15:03:59 -0500

    private

--- infoWeb/alb/alb-module-server.tf
+++ infoWeb/alb/alb-module-server.tf
@@ -1,6 +1,6 @@
 # ------------- Create ALB Target Group ----------
 resource "aws_lb_target_group" "tg_server" {
-  port     = 80
+  port     = 8080
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   health_check { 
