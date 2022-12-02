8eed13698eccfe124de1a159e5dfdce90ed3a785
Yelekeri <yelekeri@gmail.com>
2022-12-02 14:28:48 -0500

    modules

--- infoWeb/main-variables.tf
+++ infoWeb/main-variables.tf
@@ -4,6 +4,7 @@ variable "aws_region" {}
 variable "open_cidr" {}
 variable "vpc_cidr" {}
 variable "public_subnet_map" {}
+variable "private_subnet_map" {}
 
 # ALB related 
 #variable "public_subnets" {}
