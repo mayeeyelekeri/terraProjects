f758f9551e478924ccd5e5da26fe84ccc444ca05
Yelekeri <yelekeri@gmail.com>
2022-12-03 10:50:43 -0500

    private

--- modules/vpc/vpc-groups.tf
+++ modules/vpc/vpc-groups.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "public_sg" {
 }
 
 # Private Security Group
-/*resource "aws_security_group" "private-sg" {
+resource "aws_security_group" "private-sg" {
   name        = "${terraform.workspace}-private-sg"
   description = "Allow all access from public subnet"
   vpc_id      = aws_vpc.vpc.id
@@ -50,8 +50,8 @@ resource "aws_security_group" "public_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [values(aws_subnet.public)[*].cidr_block]
-  } 
+    cidr_blocks = values(aws_subnet.public)[*].cidr_block
+  }  /*
   ingress {
     description = "Allow http from public subnet"
     from_port   = 8080
@@ -72,7 +72,7 @@ resource "aws_security_group" "public_sg" {
     to_port     = -1
     protocol    = "icmp"
     cidr_blocks = [values(aws_subnet.public)[*].cidr_block]
-  } 
+  } */ 
   # ------- By default terraform doesn't create this block ----- 
   # ------- I spent lot of time to investigate this ------------
   egress {
@@ -89,4 +89,4 @@ resource "aws_security_group" "public_sg" {
   }
 
   depends_on = [aws_vpc.vpc , aws_route_table.internet_route, aws_subnet.private]
-}  */
+}  
