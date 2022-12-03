4e033016082a21a9790f7cfaf6630fdaff5d3002
Yelekeri <yelekeri@gmail.com>
2022-12-03 18:32:40 -0500

    private

--- modules/vpc/vpc-module.tf
+++ modules/vpc/vpc-module.tf
@@ -1,4 +1,9 @@
-# Create VPC 
+/* --------- Create VPC ----------------------------------
+ Inputs: 
+ 1) VPC CIDR  
+ Outputs: 
+ 1) VPC ID 
+----------------------------------------------------------- */ 
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -9,7 +14,11 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-# Create IGW and attach it to our VPC
+# 
+/* --------- Create IGW and attach it to our VPC ----------
+ Inputs: 
+ 1) VPC ID
+----------------------------------------------------------- */ 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
@@ -22,7 +31,14 @@ resource "aws_internet_gateway" "igw" {
 }
 
 # ---------------------------- PUBLIC --------------------------
-# Create Public Subnets 
+/* --------- Create Public Subnets -------------------------
+ Inputs: 
+ 1) VPC ID
+ 2) Public Subnet map 
+ 3) List of availbility zones 
+ Outputs: 
+ 1) public subnets  
+----------------------------------------------------------- */ 
 resource "aws_subnet" "public" {
   for_each = var.public_subnet_map
   vpc_id = aws_vpc.vpc.id
@@ -37,8 +53,15 @@ resource "aws_subnet" "public" {
 
   depends_on = [aws_vpc.vpc]
 }
-
-# Create route table and attach IG to it
+ 
+/* --------- Create route table and attach IG to it --------
+ Inputs: 
+ 1) VPC ID
+ 2) cidr infomation to provide access 
+ 3) Internet gateway (for internet route)
+ Outputs: 
+ 1) Route table ID   
+----------------------------------------------------------- */ 
 resource "aws_route_table" "internet_route" {
   vpc_id = aws_vpc.vpc.id
   route {
@@ -53,7 +76,13 @@ resource "aws_route_table" "internet_route" {
   depends_on = [aws_internet_gateway.igw]
 }
 
-# Associate ALL public subnets to route table 
+# 
+/* ----- Associate ALL public subnets to route table ------
+ Inputs: 
+ 1) Public subnets 
+ 2) cidr infomation to provide access 
+ 3) Route table ID 
+----------------------------------------------------------- */ 
 resource "aws_route_table_association" "public_route_table_association1" {
   for_each       = aws_subnet.public
   subnet_id      = each.value.id
@@ -63,6 +92,14 @@ resource "aws_route_table_association" "public_route_table_association1" {
 }
 
 # ---------------------------- PRIVATE --------------------------
+/* --------- Create Public Subnets -------------------------
+ Inputs: 
+ 1) VPC ID
+ 2) Private Subnet map 
+ 3) List of availbility zones 
+ Outputs: 
+ 1) Private subnets  
+----------------------------------------------------------- */ 
 # Create Public Subnets 
 resource "aws_subnet" "private" {
   for_each = var.private_subnet_map
