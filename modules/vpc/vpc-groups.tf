3efe08aee58f02f30ba23639e4ceb6d4bc393008
Yelekeri <yelekeri@gmail.com>
2022-12-03 10:43:55 -0500

    private

--- modules/vpc/vpc-groups.tf
+++ modules/vpc/vpc-groups.tf
@@ -71,7 +71,7 @@ resource "aws_security_group" "private-sg" {
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
-    cidr_blocks = [avalues(aws_subnet.public)[*].cidr_block]
+    cidr_blocks = [values(aws_subnet.public)[*].cidr_block]
   } 
   # ------- By default terraform doesn't create this block ----- 
   # ------- I spent lot of time to investigate this ------------
