30d625b5fb89c1edfaac6b533e65f6ccc3b8add2
Yelekeri <yelekeri@gmail.com>
2023-04-07 13:21:57 -0400

    codebuild

--- infoWeb/codebuild/code-build-module.tf
+++ infoWeb/codebuild/code-build-module.tf
@@ -62,7 +62,7 @@ resource "aws_codebuild_project" "server_project" {
 
     s3_logs {
       status   = "ENABLED"
-      location = "${var.codebucket_name}/server/build-log"
+      location = "s3://${var.codebucket_name}/server/build-log"
     } 
   }
 
