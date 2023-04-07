d9ccd88cef53339b8234e984e78498478e36972a
Yelekeri <yelekeri@gmail.com>
2023-04-07 11:56:49 -0400

    codebuild

--- infoWeb/main-variables.tf
+++ infoWeb/main-variables.tf
@@ -58,4 +58,6 @@ variable "info_client_port" {}
 variable "git_creds" {}
 variable "server_project_name" {} 
 variable "server_project_description" {} 
+variable "client_project_name" {} 
+variable "client_project_description" {} 
 variable "source_provider" {}
