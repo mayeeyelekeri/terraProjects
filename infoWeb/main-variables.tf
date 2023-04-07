921bf6b8c8cd06255bc62e5b3a400505c8a987a5
Yelekeri <yelekeri@gmail.com>
2023-04-07 14:39:56 -0400

    codebuild

--- infoWeb/main-variables.tf
+++ infoWeb/main-variables.tf
@@ -61,3 +61,4 @@ variable "server_project_description" {}
 variable "client_project_name" {} 
 variable "client_project_description" {} 
 variable "source_provider" {}
+variable "buildbucket_name" {}
