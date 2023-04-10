9608a40f1123991cd4253c31cf8549f01040468f
Yelekeri <yelekeri@gmail.com>
2023-04-09 22:54:53 -0400

    codebuild start

---
+++ infoWeb/alb/alb-outputs.tf
@@ -0,0 +1,15 @@
+output "alb_client_dns" {
+  value       = aws_lb.alb_client.dns_name
+}
+
+output "alb_server_dns" {
+  value       = aws_lb.alb_server.dns_name
+}
+
+output "alb_tg_server_arn" {
+  value       = aws_lb_target_group.tg_server.arn
+}
+
+output "alb_tg_client_arn" {
+  value       = aws_lb_target_group.tg_client.arn
+}
