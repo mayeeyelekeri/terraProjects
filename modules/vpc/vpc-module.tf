697e35bd8c8bcf9f0b8d4201a64d6f67caea267b
Yelekeri <yelekeri@gmail.com>
2023-04-27 22:10:15 -0400

    back to square one

--- modules/vpc/vpc-module.tf
+++ modules/vpc/vpc-module.tf
@@ -106,7 +106,7 @@ resource "aws_subnet" "private" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = each.value.cidr
   availability_zone = each.value.zone
-  # map_public_ip_on_launch = "false"
+  map_public_ip_on_launch = "true"
 
   tags = {
     Name = "${terraform.workspace}-${each.value.cidr} - ${each.value.zone} - private"
