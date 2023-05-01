0d3cf40f3fde44217196835d369262cdd4c12c7f
Yelekeri <yelekeri@gmail.com>
2023-05-01 15:03:56 -0400

    ec2publicprivate setup updated for keypair

--- ec2PublicPrivate/init.sh
+++ ec2PublicPrivate/init.sh
@@ -1,6 +1,6 @@
 aws s3 mb s3://myterraprojects --region us-east-1
 
-aws ec2 create-key-pair --key-name ec2PublicPrivate --region us-east-1 --query 'KeyMaterial' --output text > ../../awsKeyPairDir/ec2PublicPrivate.pem
+# aws ec2 create-key-pair --key-name ec2PublicPrivate --region us-east-1 --query 'KeyMaterial' --output text > ../../awsKeyPairDir/ec2PublicPrivate.pem
 
 -------  DEVELOPMENT ------------
 terraform init 
