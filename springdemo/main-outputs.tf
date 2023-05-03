b4a0b68e13f52c9b59309989b194f2ebbce58c7e
Yelekeri <yelekeri@gmail.com>
2023-05-02 21:05:06 -0400

    springdemo

--- springdemo/main-outputs.tf
+++ springdemo/main-outputs.tf
@@ -16,11 +16,6 @@ output "public" {
 	value = values(module.vpc.public)[*].cidr_block
 }
 
-
-output "nat_gateway_id" { 
-	value = module.vpc.nat_gateway_id
-} 
-
 # ------------ CodeBuild outputs -------------------
 
 output "codebuild_bucket_id" { 
