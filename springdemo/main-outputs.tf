5207d88cda4e271ec14c8e00f0f9b76c1b2f002d
Yelekeri <yelekeri@gmail.com>
2023-05-02 20:43:01 -0400

    springdemo project

--- springdemo/main-outputs.tf
+++ springdemo/main-outputs.tf
@@ -34,17 +34,10 @@ output "alb_tg_server_arn" {
 	value = module.alb.alb_tg_server_arn
 }
 
-output "alb_tg_client_arn" { 
-	value = module.alb.alb_tg_client_arn
-}
-
 output "alb_server_dns" { 
 	value = module.alb.alb_server_dns
 }
 
-output "alb_client_dns" { 
-	value = module.alb.alb_client_dns
-}
 
 /*# ----------- Autoscaling --------------------
 output "autoscale-auto_scale_group_name_server" { 
--- springdemo/main-variables.tf
+++ springdemo/main-variables.tf
@@ -12,7 +12,6 @@ variable "state_bucket" {}
 
 # Autoscaling 
 variable "app_name_server" {}
-variable "app_name_client" {}
 variable "key_name" {}
 variable "ami_id" {}
 variable "instance_type" {}
@@ -21,18 +20,11 @@ variable "autoscale_min" {}
 variable "autoscale_max" {}
 variable "app_health_check_path" {}
 variable "template_name_server" {}
-variable "template_name_client" {}
 
 # Code Deploy 
 variable "codebucket_name" {}
-#variable "app_name_server" {}
-#variable "app_name_client" {}
 variable "zip_file_server" {}
-variable "zip_file_client" {}
 variable "webapp_src_location_server" {}
-variable "webapp_src_location_client" {}
-#variable "auto_scale_group_name_server" {}
-#variable "auto_scale_group_name_client" {}
 
 # Build 
 variable "war_file_server" {}
@@ -43,23 +35,12 @@ variable "dest_properties_file_server" {}
 variable "jar_file_server" {}
 variable "info_server_workspace" {} 
 
-variable "war_file_client" {}
-#variable "app_name_client" {}
-#variable "zip_file_client" {}
-variable "docker_file_client" {}
-variable "dest_properties_file_client" {}
-variable "jar_file_client" {}
-variable "info_client_workspace" {} 
-variable "src_properties_file_client" {}
 variable "src_properties_file_server" {}
 variable "mysql_creds" {}
-variable "info_client_port" {} 
 
 # Codebuild 
 variable "git_creds" {}
 variable "server_project_name" {} 
 variable "server_project_description" {} 
-variable "client_project_name" {} 
-variable "client_project_description" {} 
 variable "source_provider" {}
 variable "buildbucket_name" {}
