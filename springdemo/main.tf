c8a6a70091bc9d19479cd787aca168b91e3e0559
Yelekeri <yelekeri@gmail.com>
2023-05-02 23:02:44 -0400

    springdemo

--- springdemo/main.tf
+++ springdemo/main.tf
@@ -118,7 +118,7 @@ module "autoscale" {
  5) Upload file to bucket 
  6) Initiate deploy 
 -------------------------------------------------------- */ 
-/*module "codedeploy" { 
+module "codedeploy" { 
     source      = "../modules/codedeploy"
 
     codebucket_name               = module.codebuild.codebuild_bucket_id
@@ -140,5 +140,5 @@ module "autoscale" {
     #  from ALB module 
     alb_server_dns               = module.alb.alb_server_dns
 
-} */ 
+} 
 
