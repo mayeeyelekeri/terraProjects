acd7a08a2db86bc1ac99b77244fb3e4e9b849494
Yelekeri <yelekeri@gmail.com>
2023-05-05 16:43:17 -0400

    awscodepipeline

--- awspipelinedemo/init.sh
+++ awspipelinedemo/init.sh
@@ -1,6 +1,6 @@
 
---- Create an S3 bucket for strong terraform state 
+# Create an S3 bucket for strong terraform state 
 aws s3 mb s3://terraprojects --region us-east-1
 
---- In database directory, execute script to create mysql creds in secrets manager 
+# In database directory, execute script to create mysql creds in secrets manager 
 . ~/INFO/secrets/aws_secrets.sh 
