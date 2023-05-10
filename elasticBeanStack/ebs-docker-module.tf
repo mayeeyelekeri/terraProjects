de04159f358beabc0268f45a9368f627cd0ec0a5
Yelekeri <yelekeri@gmail.com>
2023-05-10 09:04:41 -0400

    elb

--- elasticBeanStack/ebs-docker-module.tf
+++ elasticBeanStack/ebs-docker-module.tf
@@ -6,18 +6,18 @@ resource "aws_elastic_beanstalk_application" "dockerapp" {
   depends_on = [null_resource.upload_file, aws_iam_role_policy.ebs_policy]
 }
 
-/*resource "aws_elastic_beanstalk_application_version" "beanstalk_dockerapp_version" {
+resource "aws_elastic_beanstalk_application_version" "beanstalk_dockerapp_version" {
   application = aws_elastic_beanstalk_application.dockerapp.name
   bucket = aws_s3_bucket.codebucket.id
   key = var.dockerfile-name
   name = "${var.dockerapp-name}-1.0.0"
 
   depends_on = [aws_elastic_beanstalk_application.dockerapp]
-} */
+}
 
 
 # Create environment 
-/* resource "aws_elastic_beanstalk_environment" "dockerapp-env" {
+resource "aws_elastic_beanstalk_environment" "dockerapp-env" {
   name = "dockerapp-env"
   application = aws_elastic_beanstalk_application.dockerapp.name
   solution_stack_name = var.dockerstack-name
@@ -32,20 +32,20 @@ resource "aws_elastic_beanstalk_application" "dockerapp" {
   setting {
    namespace = "aws:autoscaling:launchconfiguration"
    name = "IamInstanceProfile"
-   value = var.instance-profile
+   value = aws_iam_instance_profile.myinstanceprofile.name
    #value = "aws-elasticbeanstalk-ec2-role"  # **** this gets created automatically from aws console when an app is created 
   }
-  */
-  /* setting {
+  
+  setting {
     name = "SERVER_PORT"
     namespace = "aws:elasticbeanstalk:application:environment"
     value = "5000"
-  } */
-  /*
+  }
+  
   depends_on = [aws_iam_role.beanstackrole, aws_iam_instance_profile.myinstanceprofile]
-} */ 
+}  
 
-/* 
+ 
 #.................................................
 # upload war file to S3 object 
 resource "null_resource" "upload_dockerfile" { 
@@ -62,4 +62,3 @@ EOF
  
   depends_on = [aws_s3_bucket.codebucket]
 } # end of "null_resource" "upload_file"
-*/
