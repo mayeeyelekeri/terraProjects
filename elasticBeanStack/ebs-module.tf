fb4b47aaad3428a317f4eb3a349a771b272530cc
Yelekeri <yelekeri@gmail.com>
2023-05-09 21:35:26 -0400

    elb

--- elasticBeanStack/ebs-module.tf
+++ elasticBeanStack/ebs-module.tf
@@ -32,7 +32,7 @@ resource "aws_elastic_beanstalk_environment" "myapp-env" {
   setting {
    namespace = "aws:autoscaling:launchconfiguration"
    name = "IamInstanceProfile"
-   value = aws_iam_role.beanstackrole.name
+   value = aws_iam_instance_profile.myinstanceprofile.name
    #value = "aws-elasticbeanstalk-ec2-role"  # **** this gets created automatically from aws console when an app is created 
   }
 
