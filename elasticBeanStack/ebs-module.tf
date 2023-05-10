af4d622a5d40230e50f0315227a24ee8803c5b0a
Yelekeri <yelekeri@gmail.com>
2023-05-09 22:12:09 -0400

    elb

--- elasticBeanStack/ebs-module.tf
+++ elasticBeanStack/ebs-module.tf
@@ -66,7 +66,7 @@ resource "aws_s3_bucket_versioning" "versioning" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.codebucket.id
 
-  block_public_acls       = false
+  #block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
