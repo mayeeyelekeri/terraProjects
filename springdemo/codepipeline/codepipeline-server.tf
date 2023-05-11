2416d7fc4f0c7962466f921bca2c01e281b8426a
Yelekeri <yelekeri@gmail.com>
2023-05-10 22:14:36 -0400

    infoweb

--- springdemo/codepipeline/codepipeline-server.tf
+++ springdemo/codepipeline/codepipeline-server.tf
@@ -1,5 +1,5 @@
 resource "aws_codepipeline" "codepipeline" {
-  name     = car.server_project_name 
+  name     = var.server_project_name 
   role_arn = aws_iam_role.codepipeline_role.arn
 
   artifact_store {
