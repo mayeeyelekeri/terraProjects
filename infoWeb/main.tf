51bd7a723869dc207b7313b2aea686f7c2c012f5
Yelekeri <yelekeri@gmail.com>
2023-06-09 13:45:59 -0400

    name for Auto scaling group

--- infoWeb/main.tf
+++ infoWeb/main.tf
@@ -152,6 +152,6 @@ module "codedeploy" {
     #  from ALB module 
     alb_server_dns               = module.alb.alb_server_dns
 
-    depends_on = [autoscale, alb]
+    depends_on = [module.autoscale, module.alb, module.codebuild]
 } 
 
