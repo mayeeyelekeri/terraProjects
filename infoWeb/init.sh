1532aef94f73cb6251ec7c8ada435aa2365681e0
Yelekeri <yelekeri@gmail.com>
2023-10-31 15:04:06 -0400

    bucket name

--- infoWeb/init.sh
+++ infoWeb/init.sh
@@ -2,7 +2,7 @@
 sudo /usr/sbin/ntpdate pool.ntp.org
 
 # Create an S3 bucket for strong terraform state 
-aws s3 mb s3://terraprojects --region us-east-1
+aws s3 mb s3://terraprojects1 --region us-east-1
 
 # In database directory, execute script to create mysql creds in secrets manager 
 . ~/INFO/secrets/aws_secrets.sh 
--- springdemo/dev/terraform.tfvars
+++ springdemo/dev/terraform.tfvars
@@ -24,7 +24,7 @@ private_subnet_map = {
 			     } # end of subnet_map private 
 
 git_creds   = "git_creds"
-state_bucket = "terraprojects"
+state_bucket = "terraprojects1"
 
 #......................................
 # EC2 Related variables 
