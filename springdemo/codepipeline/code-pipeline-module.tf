53afc462edc56cec4e590c86127bab943681b162
Mahesh Mahadeva <yelekeri@gmail.com>
2024-03-26 22:59:59 -0400

    codepipeline

--- springdemo/codepipeline/code-pipeline-module.tf
+++ springdemo/codepipeline/code-pipeline-module.tf
@@ -1,6 +1,4 @@
 
-
-
 resource "aws_codepipeline" "codepipeline" {
   name     = "${var.pipeline_name}"
   role_arn = aws_iam_role.my_code_pipeline_role.arn
@@ -9,6 +7,26 @@ resource "aws_codepipeline" "codepipeline" {
         location = aws_s3_bucket.pipelinebucket.bucket
         type     = "S3"
   }
+
+   stage {
+    name = "Source"
+
+    action {
+      name = "Source"
+      category = "Source"
+      owner = "ThirdParty"
+      provider = "GitHub"
+      version = "1"
+      output_artifacts = ["SourceArtifact"]
+
+      configuration = {
+        //OAuthToken = "${local.github_token}"
+        //Owner = "${local.github_owner}"
+        Repo = local.git_creds.springboot_git_repository
+        Branch = "main"
+      }
+    }
+  } // end of stage "Source"
 
   stage {
     name = "Build"
@@ -46,3 +64,17 @@ resource "aws_codepipeline" "codepipeline" {
     }
   } // end of stage "Deploy" 
 } 
+
+# Get Github secrets from AWS Secret Manager 
+data "aws_secretsmanager_secret_version" "creds" {
+  # Fill in the name you gave to your secret
+  secret_id = var.git_creds 
+}
+
+locals {git_creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)}
+
+resource "aws_codebuild_source_credential" "example" {
+  auth_type   = "PERSONAL_ACCESS_TOKEN"
+  server_type = "GITHUB"
+  token       = local.git_creds.token
+}
