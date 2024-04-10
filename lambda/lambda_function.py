e9257bf0bbf51f192b2223f98eb44f64fae171af
Mahesh Mahadeva <yelekeri@gmail.com>
2024-04-09 23:13:29 -0400

    lambda added

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,12 +2,13 @@ import json
 import os
 import boto3
 
-from base64 import b64decode 
-aws_client = boto3.client('ssm')
-ssm_message_decrpyted = aws_client.get_parameter( Name='message', WithDecryption=True)
+#from base64 import b64decode 
+#aws_client = boto3.client('ssm')
+#ssm_message_decrpyted = aws_client.get_parameter( Name='message', WithDecryption=True)
 
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello to Lambda , Mr.' + os.environ['myname'] + ', this is the message from Systems Manager ....' + ssm_message_decrpyted)
+        'body': json.dumps('Hello to Lambda , Mr.' + os.environ['myname'])
+        #'body': json.dumps('Hello to Lambda , Mr.' + os.environ['myname'] + ', this is the message from Systems Manager ....' + ssm_message_decrpyted)
     }
