40f8e199baab9bd28926f13eaec07935d44be93d
Mahesh Mahadeva <yelekeri@gmail.com>
2024-04-09 22:56:08 -0400

    lambda added

--- lambda/main.tf
+++ lambda/main.tf
@@ -35,7 +35,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function.zip"
   function_name = var.function_name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "lambda_handler"
+  handler       = "lambda_function.lambda_handler"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
