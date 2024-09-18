5c0e23e72f02abec8983e6a7de1155e5f95c1930
Vagrant <yelekeri@gmail.com>
2024-09-18 15:24:41 +0000

    formatted

--- infoWeb/main-outputs.tf
+++ infoWeb/main-outputs.tf
@@ -5,47 +5,47 @@ output "vpc-vpc_id" {
 }
 
 output "vpc-public_sg_id" {
-	value = module.vpc.public_sg_id
+  value = module.vpc.public_sg_id
 }
 
-output "public-subnet-ids" { 
-	value = module.vpc.public_subnets[*].id
+output "public-subnet-ids" {
+  value = module.vpc.public_subnets[*].id
 }
 
-output "public" { 
-	value = values(module.vpc.public)[*].cidr_block
+output "public" {
+  value = values(module.vpc.public)[*].cidr_block
 }
 
 
-output "nat_gateway_id" { 
-	value = module.vpc.nat_gateway_id
-} 
+output "nat_gateway_id" {
+  value = module.vpc.nat_gateway_id
+}
 
 # ------------ CodeBuild outputs -------------------
 
-output "codebuild_bucket_id" { 
-	value = module.codebuild.codebuild_bucket_id
+output "codebuild_bucket_id" {
+  value = module.codebuild.codebuild_bucket_id
 }
 
 
 # ------------ ALB outputs -------------------
 
-output "alb_tg_server_arn" { 
-	value = module.alb.alb_tg_server_arn
+output "alb_tg_server_arn" {
+  value = module.alb.alb_tg_server_arn
 }
 
-output "alb_tg_client_arn" { 
-	value = module.alb.alb_tg_client_arn
+output "alb_tg_client_arn" {
+  value = module.alb.alb_tg_client_arn
 }
 
-output "alb_server_dns" { 
-	value = module.alb.alb_server_dns
+output "alb_server_dns" {
+  value = module.alb.alb_server_dns
 }
 
-output "alb_client_dns" { 
-	value = module.alb.alb_client_dns
+output "alb_client_dns" {
+  value = module.alb.alb_client_dns
 }
- 
+
 /*# ----------- Autoscaling --------------------
 output "autoscale-auto_scale_group_name_server" { 
 	value = module.autoscale.auto_scale_group_name_server
--- infoWeb/main-variables.tf
+++ infoWeb/main-variables.tf
@@ -5,7 +5,7 @@ variable "open_cidr" {}
 variable "vpc_cidr" {}
 variable "public_subnet_map" {}
 variable "private_subnet_map" {}
-variable "state_bucket" {} 
+variable "state_bucket" {}
 
 
 # ALB related 
@@ -18,7 +18,7 @@ variable "key_name" {}
 variable "ami_id" {}
 variable "instance_type" {}
 variable "instance_profile_name" {}
-variable "autoscale_min" {} 
+variable "autoscale_min" {}
 variable "autoscale_max" {}
 variable "autoscale_desired" {}
 variable "app_health_check_path" {}
@@ -44,7 +44,7 @@ variable "war_file_server" {}
 variable "docker_file_server" {}
 variable "dest_properties_file_server" {}
 variable "jar_file_server" {}
-variable "info_server_workspace" {} 
+variable "info_server_workspace" {}
 
 variable "war_file_client" {}
 #variable "app_name_client" {}
@@ -52,17 +52,17 @@ variable "war_file_client" {}
 variable "docker_file_client" {}
 variable "dest_properties_file_client" {}
 variable "jar_file_client" {}
-variable "info_client_workspace" {} 
+variable "info_client_workspace" {}
 variable "src_properties_file_client" {}
 variable "src_properties_file_server" {}
 variable "mysql_creds" {}
-variable "info_client_port" {} 
+variable "info_client_port" {}
 
 # Codebuild 
 variable "git_creds" {}
-variable "server_project_name" {} 
-variable "server_project_description" {} 
-variable "client_project_name" {} 
-variable "client_project_description" {} 
+variable "server_project_name" {}
+variable "server_project_description" {}
+variable "client_project_name" {}
+variable "client_project_description" {}
 variable "source_provider" {}
 variable "buildbucket_name" {} 
